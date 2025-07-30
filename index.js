78346d11d9ceaabf8d86b866809c33a2e663a5ce
acupoftee <teedia31@gmail.com>
2025-07-30 18:34:21 -0400

    Fetch photos using search query

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/photos', async (req, res) => {
+    const query = req.query.query;
+    if (!query) {
+       return res.status(400).json({ error: 'Missing required search query'});
+    };
+
+    try {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query },
+            headers: { 'Authorization': `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}` },
+        });
+        res.send({ data: response.data });
+    } catch (e) {
+        console.error(e);
+    }
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}`))
