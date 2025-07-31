82db60ed2ae01fe0645cee94792f1493423bae8e
acupoftee <teedia31@gmail.com>
2025-07-30 22:24:56 -0400

    Add pagination support

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const port = process.env.PORT;
 
 app.get('/photos', async (req, res) => {
     const query = req.query.query;
+    const page = req.query.query
     if (!query) {
        return res.status(400).json({ error: 'Missing required search query'});
     };
 
     try {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
-            params: { query },
+            params: { query, page: page || 1 },
             headers: { 'Authorization': `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}` },
         });
         res.send({ data: response.data });
